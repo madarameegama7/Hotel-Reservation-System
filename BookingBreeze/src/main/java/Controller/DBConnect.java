b575dcb3c5deb66fb14959f4369e1adad297cf84
Madara <madarameegama7@gmail.com>
2024-10-24 00:41:46 +0530

    Create operation for user

---
+++ BookingBreeze/src/main/java/Controller/DBConnect.java
@@ -0,0 +1,25 @@
+package Controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnect {
+	
+	private static String url = "jdbc:mysql://localhost:3306/hotelreservation";
+	private static String userName = "root";
+	private static String password = "";
+	private static Connection con;
+	
+	public static Connection getConnection() {
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			con = DriverManager.getConnection(url, userName, password);
+		}catch(Exception e) {
+			System.out.println("Database connectivity is failed");
+		}
+		
+		return con;
+	}
+}
